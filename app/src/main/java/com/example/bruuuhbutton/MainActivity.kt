c3b14b3d6c60f8911d037611b2a9e5eb0a7b075f
taktan98@gmail.com <47135170+SotrydnikSee@users.noreply.github.com>
2020-08-05 13:43:26 +0400

    Рабочая кнопка звука

--- app/src/main/java/com/example/bruuuhbutton/MainActivity.kt
+++ app/src/main/java/com/example/bruuuhbutton/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.bruuuhbutton
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_sound.setOnClickListener { onSound() }
+    }
+    fun onSound(){
+        val text = "Будет звук Bruuh!"
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+
+        val player = MediaPlayer.create(this, R.raw.bruh)
+        player.start();
     }
 }
